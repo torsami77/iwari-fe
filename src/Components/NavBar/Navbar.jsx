13b26620b9e3d36f471332acc17221098638f48c
Xavier-Platinum <kwislawrencekwis@gmail.com>
2021-10-14 21:42:19 +0100

    Staged environment

---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Navbar.css";
+
+
+const Navbar = () => {
+    return (
+        <div className="wrapper">
+            I am Nav
+        </div>
+    )
+}
+
+export default Navbar;
