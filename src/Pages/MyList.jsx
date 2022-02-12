3976999b2666afeeb4bbcee2938a562a50d7f2b9
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-02-13 00:40:50 +0100

    updates

--- src/Pages/MyList.jsx
+++ src/Pages/MyList.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import Navbar from '../Components/NavBar/Navbar';
+import Sidebar from '../Components/Sidebar/Sidebar';
+import styled from 'styled-components';
+import Container from '../Styles/Container/Container';
+
+const DIV = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    height: 530px;
+`;
 
 const MyList = () => {
     return (
-        <div>MyList</div>
+        <div>
+            <Navbar />
+            <Sidebar />
+            <Container>
+                <DIV>
+                    <h1>This page is under maintainance check back soon.</h1>
+                </DIV>
+            </Container>
+        </div>
     )
 }
 
-export default MyList;
+export default Watch;
