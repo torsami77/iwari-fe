50252135d8676fbdbb2b992ee39dc5a162b50700
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-02-12 23:56:54 +0100

    Added folder files

---
+++ src/Styles/Themes/theme.js
@@ -0,0 +1,30 @@
+export const darkTheme = {
+  bg: "#181818",
+  primaryColor: "#FFF",
+  secondaryColor: "#AAAAAA",
+  grey: "#202020",
+  darkGrey: "#383838",
+  black: "#121212",
+  red: "#CC0000",
+  blue: "#3EA6FF",
+  white: "#FFF",
+  pink: "#F86782",
+  purple: "#282A36",
+  font: "Fira Sans",
+};
+
+export const lightTheme = {
+  bg: "#6A686C",
+  primaryColor: "#FFF",
+  secondaryColor: "#FF4D54",
+  grey: "#FFFFFF",
+  darkGrey: "#383838",
+  black: "#121212",
+  red: "#CC0000",
+  blue: "#3EA6FF",
+  white: "#FFF",
+  pink: "#F86782",
+  purple: "#282A36",
+  font: "Fira Sans",
+  input: "#F0F0F0"
+}
