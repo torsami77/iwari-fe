4e970e589540ed3e35c3e8172e5f661c078bf1cd
Xavier-Platinum <kwislawrencekwis@gmail.com>
2022-02-13 00:37:14 +0100

    Added watch file

--- src/Pages/Watch.jsx
+++ src/Pages/Watch.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Navbar from '../Components/NavBar/Navbar';
+import Sidebar from '../Components/Sidebar/Sidebar';
+import styled from 'styled-components';
+import Container from '../Styles/Container/Container';
+
+const DIV = styled.div `
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 1px solid black;
+    height: 530px;
+`;
 
 const Watch = () => {
     return (
-        <div>Watch</div>
+        <div>
+            <Navbar />
+            <Sidebar />
+            <Container>
+                <DIV>
+                    <h1>This page is under maintainance check back soon.</h1>
+                </DIV>
+            </Container>
+        </div>
     )
 }
 
